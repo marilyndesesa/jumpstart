970d6b8db18f1934512946e9c9fe3d2dbd899bbb
Marilyn DeSesa <marilyn.desesa@gmail.com>
2017-10-30 17:34:32 -0700

    added js to hide menu on scroll

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,9 +1,22 @@
-$(document).ready(function () {
+$(document).ready(function() {
   $('.carousel').slick({
   	arrows: false,
     autoplay: true,
     autoplaySpeed: 4000,
     pauseOnFocus: false,
     pauseOnHover: false
+  });
+
+  var scrollLength = 0
+  $(window).scroll(function() {
+    var scrollToTop = $(this).scrollTop();
+    if (scrollToTop - scrollLength > 50) {
+      var navHeight = $('.navbar').css('height');
+      $('.navbar').animate({top: '-' + navHeight}, 150);
+      scrollLength = scrollToTop;
+    } else if (scrollLength - scrollToTop > 50) {
+      $('.navbar').animate({top: '0px'}, 50);
+      scrollLength = scrollToTop;
+    }
   });
 });
