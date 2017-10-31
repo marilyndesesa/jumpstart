7b2a78c109b1b49a7ab861781e8ebff664e1a5c4
Marilyn DeSesa <marilyn.desesa@gmail.com>
2017-10-30 17:16:53 -0700

    added slick plugin

---
+++ resources/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  $('.carousel').slick({
+  	arrows: false,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    pauseOnFocus: false,
+    pauseOnHover: false
+  });
+});
